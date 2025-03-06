5bb24f691094c1f760c8db2bedf79085d8d858a9
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2025-03-06 15:59:43 +0200

    coded js for the initialization of portfolio data in the config.js file starting with the about

---
+++ config.js
@@ -0,0 +1,8 @@
+const portfolioData = {
+    about: `Hello! I'm a software developer and engineer passionate about 
+    creating interactive software.
+    I specialize in backend development with focus on cloud and software integration.
+    Currently based in Johannesburg, South Africa, I'm always open to new
+    oppurtunities and collaborations.`,
+
+}
