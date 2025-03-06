7e3f3d71d49ce9558350a6ac70c6412b1d90b3c6
Bonolo Aphane <baphane023@student.wethinkcode.co.za>
2025-03-06 16:32:09 +0200

    coded js in main to initialize DOM elements

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import { portfolioData, glitchFrames, defaultDarkMode } from "./config";
+
+const loadingText = document.getElementById("loading");
+const terminal = document.getElementById("terminal");
+const terminalInput = document.getElementById("terminal-input");
+const commandHistory = document.getElementById("command-history");
+const themeToggle = document.getElementById("theme-toggle");
+const moonIcon = document.querySelector(".moon")
+const sunIcon = document.querySelector(".sun");
+const terminalContent = document.querySelector(".terminal-content");
